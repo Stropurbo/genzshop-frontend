472970f784f3649a5628c1d5ceca97f8b4935962
Sabbir Hasan <xofficial490@gmail.com>
2025-05-08 23:40:09 +0600

    bug fix

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -74,7 +74,7 @@ const Navbar = () => {
 					</svg>
 				</button> */}
 
-				<BiHeart className="h-5 w-5" cursor={PointerEvent}/>
+				<BiHeart className="h-5 w-5 hover:bg-amber-500"/>
 
 				{user ? (
 					<div className="flex">
