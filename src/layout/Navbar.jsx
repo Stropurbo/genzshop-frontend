e76cf6e5599af4b9e70710a7ccb76a1aa437a23c
Sabbir Hasan <xofficial490@gmail.com>
2025-05-08 23:29:05 +0600

    bug fix

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link, NavLink } from 'react-router'
 import useAuthContext from '../hooks/useAuthContext'
 import useCartContext from '../hooks/useCartContext'
+import { BiHeart } from 'react-icons/bi'
 
 const Navbar = () => {
 	const { user, logoutUser } = useAuthContext()
@@ -54,7 +55,26 @@ const Navbar = () => {
 				</Link>
 			</div>
 
-			<div className="navbar-end">				
+			<div className="navbar-end">
+				{/* <button className="btn btn-ghost btn-circle">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						className="h-5 w-5"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+					>
+						{' '}
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+						/>{' '}
+					</svg>
+				</button> */}
+
+				<BiHeart />
 
 				{user ? (
 					<div className="flex">
