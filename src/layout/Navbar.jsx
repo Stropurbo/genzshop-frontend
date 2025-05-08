b27871147b3bbb001c607fdac1c49f378035af73
Sabbir Hasan <xofficial490@gmail.com>
2025-05-08 23:38:02 +0600

    bug fix

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -162,7 +162,7 @@ const Navbar = () => {
 						</div>
 					</div>
 				) : (
-					<div className="flex gap-2 ms-5">
+					<div className="flex gap-2 ms-4">
 						<Link
 							to="/login"							
 						>
