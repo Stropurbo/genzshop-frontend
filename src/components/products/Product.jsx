e8796314aa20be22b35bee88012da261edf51d25
Sabbir Hasan <xofficial490@gmail.com>
2025-05-09 11:30:58 +0600

    bug fix

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -24,54 +24,93 @@ const Product = () => {
 	}, [])
 
 	return (
-		<section className="mx-auto py-15">
-			<div className="flex justify-between px-5 md:px-8 items-center">
-				<h1 className="font-bold text-xl md:text-4xl">BestSellers</h1>
-				<Link to="/all-product">Explore</Link>
+		<div>
+			<div className="mx-auto py-15">
+				<div className="flex justify-between px-5 md:px-8 items-center">
+					<h1 className="font-bold text-xl md:text-4xl">BestSellers</h1>
+					<Link to="/all-product">Explore</Link>
+				</div>
+
+				{isloading && (
+					<div className="flex justify-center items-center">
+						<span className="loading loading-spinner loading-lg text-center m-5 "></span>
+					</div>
+				)}
+
+				{error && <ErrorAlert errormessage={'Product Load Failed!'} />}
+
+				{!isloading && !error && product.length > 0 && (
+					<Swiper
+						modules={[Autoplay, Pagination, Navigation]}
+						spaceBetween={2}
+						centeredSlides={false}
+						autoplay={{
+							delay: 2500,
+							disableOnInteraction: false,
+						}}
+						// pagination={{
+						//   clickable: true,
+						// }}
+						pagination={false}
+						navigation={false}
+						slidesPerView={1}
+						breakpoints={{
+							640: { slidesPerView: 2 },
+							1024: { slidesPerView: 3 },
+						}}
+					>
+						{product.map((product) => (
+							<SwiperSlide
+								key={product.id}
+								className="flex justify-center"
+							>
+								<ProductItem product={product} />
+							</SwiperSlide>
+						))}
+					</Swiper>
+				)}
+
+				{!isloading && !error && product.length === 0 && (
+					<p className="text-center text-gray-500 font-bold ">
+						Product Not Available
+					</p>
+				)}
 			</div>
 
-			{isloading && (
-				<div className="flex justify-center items-center">
-					<span className="loading loading-spinner loading-lg text-center m-5 "></span>
-				</div>
-			)}
-
-			{error && <ErrorAlert errormessage={'Product Load Failed!'} />}
-
-			{!isloading && !error && product.length > 0 && (
-				<Swiper
-					modules={[Autoplay, Pagination, Navigation]}
-					spaceBetween={2}
-					centeredSlides={false}
-					autoplay={{
-						delay: 2500,
-						disableOnInteraction: false,
-					}}
-					// pagination={{
-					//   clickable: true,
-					// }}
-					pagination={false}
-					navigation={false}
-					slidesPerView={1}
-					breakpoints={{
-						640: { slidesPerView: 2 },
-						1024: { slidesPerView: 3 },
-					}}
-				>
-					{product.map((product) => (
-						<SwiperSlide
-							key={product.id}
-							className="flex justify-center"
-						>
-							<ProductItem product={product} />
-						</SwiperSlide>
-					))}
-				</Swiper>
-			)}
-			{!isloading && !error && product.length === 0 && (
-				<p className="text-center text-gray-500 font-bold ">Product Not Available</p>
-			)}
-		</section>
+			<p>Weekly Featured Products</p>
+			<div>
+				{!isloading && !error && product.length > 0 && (
+					<Swiper
+						modules={[Autoplay, Pagination, Navigation]}
+						spaceBetween={2}
+						centeredSlides={false}
+						autoplay={{
+							delay: 2500,
+							disableOnInteraction: false,
+						}}
+						// pagination={{
+						//   clickable: true,
+						// }}
+						pagination={false}
+						navigation={false}
+						slidesPerView={1}
+						breakpoints={{
+							640: { slidesPerView: 2 },
+							1024: { slidesPerView: 3 },
+						}}
+					>
+						{product.map((product) => (
+							<SwiperSlide
+								key={product.id}
+								className="flex justify-center"
+							>
+								<ProductItem product={product} />
+							</SwiperSlide>
+						))}
+					</Swiper>
+				)}
+			</div>
+		</div>
 	)
 }
 
