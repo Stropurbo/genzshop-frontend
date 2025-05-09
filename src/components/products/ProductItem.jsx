6109b5318bf20cf39b8d54a4d33fc5f299289d35
Sabbir Hasan <xofficial490@gmail.com>
2025-05-09 11:20:13 +0600

    bug fix

--- src/components/products/ProductItem.jsx
+++ src/components/products/ProductItem.jsx
@@ -3,7 +3,7 @@ import default_image from '../../assets/default_product.jpg'
 const ProductItem = ({ product }) => {
 	return (
 		<a href={`/shop/${product.id}`}>
-			<div className="card bg-base-100 w-96 shadow-sm">
+			<div className="card bg-base-100 w-96 shadow-sm group">
 				<figure className="px-10 pt-10 h-[200px]">
 					<img
 						src={
@@ -13,12 +13,12 @@ const ProductItem = ({ product }) => {
 						className="rounded-xl"
 					/>
 				</figure>
-				<div className="card-body items-center text-center hover:shadow-xl transition ">
+				<div className="card-body items-center text-center">
 					<h2 className="card-title">{product.name}</h2>
 					<p className="line-clamp-1">{product.description}</p>
 					<p className="font-bold"> ${product.price} </p>
 
-					<div className="card-actions mt-4 opacity-0 group-hover:opacity-100 transition duration-300">
+					<div className="card-actions mt-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
 						<button className="btn btn-primary">Buy Now</button>
 					</div>
 				</div>
